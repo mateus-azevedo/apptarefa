c3f1e9cc5a341040485767475b24ce47c07afa59
Mateus Santos <m.santos.azeve@gmail.com>
2021-06-07 21:56:19 -0300

    feature/home: Estudo da FlatList

--- src/features/home/components/panel.tsx
+++ src/features/home/components/panel.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, FlatList} from 'react-native';
+
+const produtos = [
+  {
+    id: '001',
+    desc: ['Mouse', '25.00'],
+  },
+  {
+    id: '002',
+    desc: ['Teclado', '50.00'],
+  },
+  {
+    id: '003',
+    desc: ['Monitor', '430.00'],
+  },
+];
 
 const HomePanel = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={styles.container}>
       <Text>HomePanel Conquistado</Text>
+      <FlatList
+        data={produtos}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <Text>
+            Descrição: {item.desc[0]} - Valor:{item.desc[1]}
+          </Text>
+        )}
+      />
     </View>
   );
 };
 
 export default HomePanel;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
