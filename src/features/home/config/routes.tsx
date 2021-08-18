8f9133e008572e3c9f1ed1aa055004b2970685d2
Mateus Santos <m.santos.azeve@gmail.com>
2021-08-18 16:46:13 -0300

    fix: correção importações

--- src/features/home/config/routes.tsx
+++ src/features/home/config/routes.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import * as Screen from '../screens';
+import * as Screens from '../screens';
 
 const Stack = createStackNavigator();
 
 const Routes = () => {
   return (
     <Stack.Navigator headerMode="none">
-      <Stack.Screen name="HomePanel" component={Screen.HomePanel} />
+      <Stack.Screen name="HomePanel" component={Screens.HomePanel} />
     </Stack.Navigator>
   );
 };
--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,6 +1,5 @@
 import * as Components from './components';
 import * as Config from './config';
-import * as Containers from './containers';
 import * as Screens from './screens';
 
-export {Components, Config, Containers, Screens};
+export {Components, Config, Screens};
