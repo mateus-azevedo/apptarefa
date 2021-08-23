0321630a79f7f0d0a627653b93382df11ea5bce3
Mateus Santos <m.santos.azeve@gmail.com>
2021-08-23 20:29:01 -0300

    William dica

--- src/features/welcome/screens/presentationSwiper.tsx
+++ src/features/welcome/screens/presentationSwiper.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Swiper from 'react-native-swiper';
+import {useNavigation} from '@react-navigation/native';
 
 import {Introduction, TaskTip, ToHomeScreen} from '.';
 
 const PresentationSwiper = () => {
+  const navigation = useNavigation();
   return (
     <Swiper loop={false}>
-      <Introduction />
-      <TaskTip />
-      <ToHomeScreen />
+      <Introduction header={''} image={} />
+      <TaskTip header={''} image={} footer={''} />
+      <ToHomeScreen
+        header={''}
+        image={}
+        onPress={() => navigation.navigate('home')}
+      />
     </Swiper>
   );
 };
