2f4a9609a0c8a12938523a9a5476e351ec56429d
Mateus Santos <14965085728@rio.rj.gov.br>
2021-08-26 16:58:29 -0300

    feat: implementando o styled-components

---
+++ src/features/welcome/components/Panel/index.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyledPanel, Title} from './style';
+
+const Panel = () => {
+  return (
+    <StyledPanel>
+      <Title>
+    </StyledPanel>
+  );
+};
+
+export default Panel;
---
+++ src/features/welcome/components/Panel/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const StyledPanel = styled.View({
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+export const;
