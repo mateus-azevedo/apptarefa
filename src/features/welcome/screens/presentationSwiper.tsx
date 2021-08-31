feda8cb66481edad9b4c2411fa605ea01614ca52
Mateus Santos <m.santos.azeve@gmail.com>
2021-08-30 23:12:48 -0300

    feat: componente reutilizavel
    a lib react-native-swiper não suporta a solução do styled-components. Procurar outra solução

--- src/features/welcome/screens/presentationSwiper.tsx
+++ src/features/welcome/screens/presentationSwiper.tsx
@@ -1,20 +1,24 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Swiper from 'react-native-swiper';
-import {useNavigation} from '@react-navigation/native';
+// import {useNavigation} from '@react-navigation/native';
 
-import {Introduction, TaskTip, ToHomeScreen} from '.';
+// import {Introduction, TaskTip, ToHomeScreen} from '.';
+import Panel from '../components/panel';
 
 const PresentationSwiper = () => {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
+
   return (
     <Swiper loop={false}>
-      <Introduction header={''} image={} />
+      <Panel title={'Teste Title1'} footer={'Teste Footer1'} />
+      <Panel title={'Teste Title2'} footer={'Teste Footer2'} />
+      {/* <Introduction header={''} image={} />
       <TaskTip header={''} image={} footer={''} />
       <ToHomeScreen
         header={''}
         image={}
         onPress={() => navigation.navigate('home')}
-      />
+      /> */}
     </Swiper>
   );
 };
