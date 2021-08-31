5571515174e163654911a9bbfa660c4e7500d811
Mateus Santos <m.santos.azeve@gmail.com>
2021-08-30 23:05:39 -0300

    feat: movendo os panéis da pasta screen para pasta component

--- src/features/welcome/screens/index.ts
+++ src/features/welcome/screens/index.ts
@@ -1,6 +1,3 @@
-import Introduction from './introduction';
 import PresentationSwiper from './presentationSwiper';
-import TaskTip from './taskTip';
-import ToHomeScreen from './toHomeScreen';
 
-export {Introduction, PresentationSwiper, TaskTip, ToHomeScreen};
+export {PresentationSwiper};
