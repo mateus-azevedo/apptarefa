37a309c6c6935696dd2913b3491588b32d038e06
Mateus Santos <m.santos.azeve@gmail.com>
2021-10-01 23:51:05 -0300

    estilo do componente task

--- src/features/home/components/Task/index.tsx
+++ src/features/home/components/Task/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from './style';
+import {Container, ItemLeft, Square, ItemText, Circular} from './style';
 
 interface ITaskProps {
   text: string;
@@ -7,29 +7,15 @@ interface ITaskProps {
 
 const Task = ({text}: ITaskProps) => {
   return (
-    <View>
-      <Text>{text}</Text>
-    </View>
+    <Container>
+      <ItemLeft>
+        <Square />
+        <ItemText>{text}</ItemText>
+      </ItemLeft>
+
+      <Circular />
+    </Container>
   );
 };
 
 export default Task;
-// const Item = ({item, onPress, backgroundColor, textColor}) => (
-//   <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-//     <Text style={[styles.title, textColor]}>{item.title}</Text>
-//   </TouchableOpacity>
-// );
-
-// const renderItem = ({item}) => {
-//   const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
-//   const color = item.id === selectedId ? 'white' : 'black';
-
-//   return (
-//     <Item
-//       item={item}
-//       onPress={() => setSelectedId(item.id)}
-//       backgroundColor={{backgroundColor}}
-//       textColor={{color}}
-//     />
-//   );
-// };
