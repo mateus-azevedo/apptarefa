ad9ac5607e678136301ea6c121e2816939320ad4
Mateus Santos <m.santos.azeve@gmail.com>
2021-11-01 18:43:58 -0300

    configurando pastas raiz do projeto e o typescript

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        alias: {
+          '@architecture': './src/architecture',
+          '@config': './src/config',
+          '@features': './src/features',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts'],
 };
