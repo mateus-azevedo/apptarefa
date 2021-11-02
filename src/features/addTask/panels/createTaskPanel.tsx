b898119e3e53d9b2876f11c400d47801a32702ae
Mateus Santos <m.santos.azeve@gmail.com>
2021-11-01 21:04:22 -0300

    implementado exemplo formik no projeto

--- src/features/addTask/panels/createTaskPanel.tsx
+++ src/features/addTask/panels/createTaskPanel.tsx
@@ -1,22 +1,77 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import {Formik} from 'formik';
 
-interface ICreateTaskPanelProps {}
+import {BackgroundStyled} from '@architecture/components';
 
-const CreateTaskPanel = (props: ICreateTaskPanelProps) => {
+interface ICreateTaskPanelProps {
+  taskTitle: string;
+  date?: Date;
+  description?: string;
+  labels?: Array<string>;
+}
+
+const CreateTaskPanel = ({
+  taskTitle,
+  date,
+  description,
+  labels,
+}: ICreateTaskPanelProps) => {
+  const values = {
+    taskTitle,
+    date,
+    description,
+    labels,
+  };
+
   return (
-    <Container>
-      <Text>Hello World</Text>
-    </Container>
+    <BackgroundStyled>
+      <Container>
+        <SectionTitle>Create a new Task</SectionTitle>
+        <Formik initialValues={values} onSubmit={values => console.log(values)}>
+          {({handleChange, handleBlur, handleSubmit, values}) => (
+            <Content>
+              <FormTitle>Title</FormTitle>
+              <FormInput
+                onChangeText={handleChange('taskTitle')}
+                onBlur={handleBlur('taskTitle')}
+                value={values.taskTitle}
+              />
+
+              <FormButton title="New Task" onPress={handleSubmit} />
+            </Content>
+          )}
+        </Formik>
+      </Container>
+    </BackgroundStyled>
   );
 };
 
 export default CreateTaskPanel;
 
 const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+  padding-top: 20px;
+  padding-horizontal: 20px;
 `;
 
-const Text = styled.Text``;
+const SectionTitle = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const Content = styled.View`
+  margin-top: 30px;
+`;
+
+const FormTitle = styled.Text`
+  font-size: 20px;
+`;
+
+const FormInput = styled.TextInput`
+  height: 40px;
+  margin: 12px;
+  border-width: 1px;
+  padding: 10px;
+`;
+
+const FormButton = styled.Button``;
